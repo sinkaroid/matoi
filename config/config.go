697ce5d5c4c7cff3dddf33bc7d0260858766a426
SNEK WAIFU <bibsindra@gmail.com>
2026-06-22 09:04:49 +0700

    chore(release): 10.2.1-alpha

--- config/config.go
+++ config/config.go
@@ -15,7 +15,7 @@ import (
 
 // AppVersion defines the current version of the application.
 // Can be overridden at build time using -ldflags="-X 'matoi/config.AppVersion=xxx'"
-var AppVersion = "10.2.0-alpha"
+var AppVersion = "10.2.1-alpha"
 
 // Config holds all the configuration variables for the application.
 type Config struct {
